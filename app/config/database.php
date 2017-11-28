9b458c49d24c5c5058303a9512236a53961102c5
Yuvaraj_Mudaliar <yuvim86@gmail.com>
2017-11-28 22:15:20 +0530

    Dbc

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'fusionmate',
+			'database'  => 'scrutinizer',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
