8b7767e7f5ce8544a215c1bfa93d245fa4f67326
Yuvaraj_Mudaliar <yuvim86@gmail.com>
2017-11-28 22:28:11 +0530

    dbc

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'scrutinizer',
+			'database'  => '',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
